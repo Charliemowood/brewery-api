98543de96733aa9a9a2672db430101e919ffb053
Charlie Wood <charliemowood@gmail.com>
2017-06-21 19:47:05 +0100

    Add list beverage name functionality with populate list

--- public/app.js
+++ public/app.js
@@ -8,8 +8,20 @@ var onLoad = function(){
   var jsonString = request.responseText;
   console.log(jsonString);
   var beers = JSON.parse(jsonString);
-  
-});
+  var list = populateList(beers);
+  });
 }
+
+var populateList = function(beers) {
+  var div = document.getElementById('main');
+  beers.forEach(function(beer) {
+    var li = document.createElement('li');
+    li.innerText = "Beer name: " + beer.name;
+    div.appendChild(li);
+  });
+
+  return div;
+}
+
 
 window.addEventListener('load', onLoad);
